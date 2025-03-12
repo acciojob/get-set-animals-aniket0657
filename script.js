89ddfe69856d478ac9f261c7d15687d7ac0da008
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-03-13 00:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 class Animal {
-    constructor(species) {
-        this._species = species;
-    }
+  constructor(species) {
+    this._species = species;
+  }
 
-    get species() {
-        return this._species;
-    }
+  get species() {
+    return this._species;
+  }
 
-    makeSound() {
-        console.log(`The ${this._species} makes a sound`);
-    }
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
 
 class Cat extends Animal {
-    constructor(species) {
-        super(species);
-    }
+  constructor(species) {
+    super(species);
+  }
 
-    purr() {
-        console.log("purr");
-    }
+  purr() {
+    console.log("purr");
+  }
 }
 
 class Dog extends Animal {
-    constructor(species) {
-        super(species);
-    }
+  constructor(species) {
+    super(species);
+  }
 
-    bark() {
-        console.log("woof");
-    }
+  bark() {
+    console.log("woof");
+  }
 }
-
-// Example usage
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr();      // Output: purr
-
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark();      // Output: woof
